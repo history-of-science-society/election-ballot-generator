ee90eb814b7e15d889b7a9ab9795eb79d4a35c74
Ryan Feigenbaum <rfeigenb@ND.EDU>
2020-03-09 12:49:29 -0400

    building out system

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const path = require("path");
 require("dotenv").config();
 const ejs = require("ejs");
 
+// Get data
+const objects = require("./src/data.json");
+
+// Get template
 const str = fs.readFileSync("src/index.ejs", "utf-8");
-const data = { test: "Hiya, Buddy" };
+
+// Compile template and render data
 let template = ejs.compile(str);
-const compiled = template(data);
+const compiled = template(objects);
 fs.writeFile(path.join("dist", "index.html"), compiled, err => {
   if (err) return console.log(err);
-  console.log(compiled + " > index.html");
+  console.log("Election data > index.html");
 });
